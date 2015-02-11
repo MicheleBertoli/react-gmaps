39ea9f8986c3313f234284322935417f60d43e05
Michele Bertoli <michele@berto.li>
2015-02-11 23:43:15 +0000

    heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/demo'));
+
+var host = process.env.IP || '0.0.0.0',
+  port = process.env.PORT || 3000;
+
+var server = app.listen(port, host, function() {
+  console.log('Listening at http://%s:%s', host, port);
+});
