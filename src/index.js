b3448a85040259d3338d1367f0805639fe07c91f
Michele Bertoli <michele@berto.li>
2015-08-13 09:17:47 +0100

    renamed info-window

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Gmaps from './components/gmaps';
 import Marker from './components/marker';
-import InfoWindow from './components/infoWindow';
+import InfoWindow from './components/info-window';
 
 export {Gmaps, Marker, InfoWindow};
