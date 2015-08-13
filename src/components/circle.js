e8690df87bd1e7791e5c5f5b095007462517379a
Michele Bertoli <michele@berto.li>
2015-08-13 09:23:12 +0100

    circle component

---
+++ src/components/circle.js
@@ -0,0 +1,4 @@
+import createEntity from './entity';
+import {CircleEvents} from '../utils/events';
+
+export default createEntity('Circle', CircleEvents);
--- src/utils/events.js
+++ src/utils/events.js
@@ -51,3 +51,19 @@ export const InfoWindowEvents = {
   onPositionChanged: 'position_changed',
   onZindexChanged: 'zindex_changed'
 };
+
+export const CircleEvents = {
+  onCenterChanged: 'center_changed',
+  onClick: 'click',
+  onDblClick: 'dblclick',
+  onDrag: 'drag',
+  onDragEnd: 'dragend',
+  onDragStart: 'dragstart',
+  onMouseDown: 'mousedown',
+  onMouseMove: 'mousemove',
+  onMouseOut: 'mouseout',
+  onMouseOver: 'mouseover',
+  onMouseUp: 'mouseup',
+  onRadiusChanged: 'radius_changed',
+  onRightClick: 'rightclick'
+};
