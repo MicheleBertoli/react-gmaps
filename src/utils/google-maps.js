6ede274f56634d509f4fe0871078248afdeebdd3
Adam Willoughby-Knox <adam.w.knox@gmail.com>
2016-12-15 15:41:32 -0500

    Add checks to see if the google lib has loaded
    Ids are added to the global scope, so if you have `<div id="google" />` it will bipass this check
    
    http://www.2ality.com/2012/08/ids-are-global.html
    http://stackoverflow.com/questions/9228958/how-to-check-if-google-maps-api-is-loaded

--- src/utils/google-maps.js
+++ src/utils/google-maps.js
@@ -8,7 +8,7 @@ export default {
 
   load(params, callback) {
     const index = this.callbacks.push(callback);
-    if (window.google) {
+    if (typeof window.google === 'object' && typeof window.google.maps !== 'undefined') {
       setTimeout(this.fireCallbacks.bind(this));
     } else {
       if (!this.appended) {
