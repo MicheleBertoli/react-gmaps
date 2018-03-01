afbe8ac020092506c17c26f761473434388965e4
Mauricio Maroto Arrieta <marotoam@aresep.go.cr>
2018-03-01 10:07:39 -0600

    map and markers work

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import createReactClass from 'create-react-class';
-import {Gmaps} from '../dist';
+import { Gmaps } from '../dist';
+import { Marker } from '../dist';
 import MapEvents from '../dist/events/map';
 
 const styles = {
@@ -14,8 +15,12 @@ const styles = {
   }
 };
 
-const App = createReactClass({
+const params = {
+  v: '3.exp',
+  key: 'AIzaSyDOb-PSgY3KZm8g_gjIJ2at91bSfA21z84'
+};
 
+const App = createReactClass({
   handler(_event) {
     const item = ReactDOM.findDOMNode(this.refs[_event]);
     item.style.backgroundColor = '#99ccff';
@@ -25,7 +30,6 @@ const App = createReactClass({
   },
 
   render() {
-
     const events = [];
     const handlers = {};
     for (let _event in MapEvents) {
@@ -45,18 +49,19 @@ const App = createReactClass({
           width={'50%'}
           height={'500px'}
           style={styles.cols}
-          lat={51.5258541}
-          lng={-0.08040660000006028}
+          lat={51.5}
+          lng={-0.08}
           zoom={12}
-          {...handlers} />
-        <ul style={styles.cols}>
-          {events}
-        </ul>
+          params={params}
+          {...handlers}
+        >
+          <Marker lat={51.5} lng={-0.08} icon={'https://cdn.filestackcontent.com/eAYlntJTS5yscafiMdf2'} />
+        </Gmaps>
+
+        <ul style={styles.cols}>{events}</ul>
       </div>
     );
-
   }
-
 });
 
 ReactDOM.render(<App />, document.getElementById('gmaps'));
